6f126b220e1d323228f8c793a513d12f0895dfc5
Moonsun23 <socoooool90@gmail.com>
2023-06-28 18:36:15 +0900

    Member에 기본 생성자 추가

--- src/main/java/hi1237/hello/spring4/controller/MemberController.java
+++ src/main/java/hi1237/hello/spring4/controller/MemberController.java
@@ -1,21 +1,23 @@
 package hi1237.hello.spring4.controller;
 
 
+import hi1237.hello.spring4.model.Member;
+import hi1237.hello.spring4.service.MemberService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 @Controller
 public class MemberController {
 
         // 로거 설정: getLogger(로깅할 클래스 명)
     private Logger logger = LogManager.getLogger(MemberController.class);
-
-        // http://localhost:8080/ 호출시 실행
+    @Autowired
+    private MemberService msrv;
         @RequestMapping(value="/member/join",method= RequestMethod.GET )
         public String join(Model m) {
 
@@ -28,16 +30,20 @@ public class MemberController {
         }
 
     @RequestMapping(value="/member/join",method= RequestMethod.POST )
-    public String joinok(Model m) {
-
+    public String joinok(Member m) {
         // 로거 출력
         logger.info("member/joinok 호출!");
+
+        String viewName= "/member/fail";
+        if(msrv.saveMember(m))
+            viewName = "redirect: /member/login";
+            //회원가입처리
 
         // return "index"; // jsp:view resolver
         return "redirect: /member/login";
 
     }
-    @RequestMapping(value="/member/login", method=RequestMethod.POST)
+    @RequestMapping(value="/member/login", method=RequestMethod.GET)
     public String login(Model m) {
 
         // 로거 출력
