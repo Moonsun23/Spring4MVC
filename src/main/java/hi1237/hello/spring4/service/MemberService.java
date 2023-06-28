7754b9489cbaf2c28c3fbf9b8ac62b9b730b4d31
Moonsun23 <socoooool90@gmail.com>
2023-06-28 18:37:09 +0900

    회원가입 처리 - service

---
+++ src/main/java/hi1237/hello/spring4/service/MemberService.java
@@ -0,0 +1,9 @@
+package hi1237.hello.spring4.service;
+
+import hi1237.hello.spring4.model.Member;
+
+public interface MemberService {
+
+    boolean saveMember(Member m);
+
+}
---
+++ src/main/java/hi1237/hello/spring4/service/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package hi1237.hello.spring4.service;
+
+import hi1237.hello.spring4.dao.MemberDAO;
+import hi1237.hello.spring4.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("msrv")
+public class MemberServiceImpl implements MemberService{
+// MemberServiceImpl에 대한 객체명을 msrv로 해줌
+    @Autowired
+    private MemberDAO mdao;
+    @Override
+    public boolean saveMember(Member m) {
+        boolean isSaved = false;
+        if ( mdao.insertMember(m) > 0)  isSaved = true;
+        return isSaved;
+    }
+
+}
