863630456c10090c5294786a96178a2545e9d040
Moonsun23 <socoooool90@gmail.com>
2023-06-29 12:09:11 +0900

    MemberService unit Test(단위테스트) 추가

---
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -0,0 +1,35 @@
+package service;
+
+import hi1237.hello.spring4.dao.MemberDAO;
+import hi1237.hello.spring4.model.Member;
+import hi1237.hello.spring4.service.MemberService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+        "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class MemberServiceUnitTest {
+
+    @Autowired MemberService msrv;
+
+    @Test
+    @Transactional
+
+    public void saveMember() throws Exception {
+        Member m = new Member(null, "abc123b", "987xyz", "abc123","abc123@987xyz.co.kr",null);
+        // assertEquals(테스트메서드, 검증값)
+        // abc123이라는 것(userid)에 unique 키를 넣어놔서 같은 값을 쓰면 안된다.
+        assertEquals(msrv.saveMember(m),  true);
+
+    }
+
+}
