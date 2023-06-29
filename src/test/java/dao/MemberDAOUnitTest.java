558a2be7c2babdaa95a862d21ffb3bb752c8e245
Moonsun23 <socoooool90@gmail.com>
2023-06-29 15:20:15 +0900

    assertNotNull 추가

--- src/test/java/dao/MemberDAOUnitTest.java
+++ src/test/java/dao/MemberDAOUnitTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/servlet-context.xml",
@@ -30,6 +31,20 @@ public class MemberDAOUnitTest {
         // abc123이라는 것(userid)에 unique 키를 넣어놔서 같은 값을 쓰면 안된다.
         assertEquals(mdao.insertMember(m), 1);
         // 위를 실행하면 1이 넘어온다..
+    }
+
+    @Test
+
+    public void loginMember() throws Exception {
+        Member m = new Member();
+            m.setUserid("abc123");
+            m.setPasswd("987xyz");
+
+        // assertEquals(테스트메서드, 검증값)
+
+        // System.out.println(mdao.loginMember(m));
+        assertNotNull(mdao.loginMember(m));
+        // sout문이 실행되면 null이 아니냐? 라는 뜻 - 검증함수
     }
 
 }
