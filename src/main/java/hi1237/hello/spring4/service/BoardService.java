c555ed55e58a8f1c7c2babfc5ddd5c9b6f4845ac
Moonsun23 <socoooool90@gmail.com>
2023-06-30 16:53:16 +0900

    BoardService: readBoard 구현

---
+++ src/main/java/hi1237/hello/spring4/service/BoardService.java
@@ -0,0 +1,11 @@
+package hi1237.hello.spring4.service;
+
+import hi1237.hello.spring4.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+  // 서비스에서는...  현재페이지를 넘겨주면 시작페이지를 기준으로 .....?????
+    List<Board> readBoard(int cpg);
+}
