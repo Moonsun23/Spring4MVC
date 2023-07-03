742a8f5f1df7e8b7eb9d2d0e666884bb1aed0f71
Moonsun23 <socoooool90@gmail.com>
2023-07-03 16:59:33 +0900

    join에 대해 포인트컷, 어드바이스 추가

--- src/main/java/hi1237/hello/spring4/aop/MemberServiceAdvice.java
+++ src/main/java/hi1237/hello/spring4/aop/MemberServiceAdvice.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
@@ -18,8 +19,16 @@ public class MemberServiceAdvice {
 
     private Logger logger = LogManager.getLogger(MemberServiceAdvice.class);
 
+    @Pointcut("execution(* hi1237.hello.spring4.controller.MemberController.myinfo(..))")
+        public void myInfoPoint() {}
+
+    @Pointcut("execution(* hi1237.hello.spring4.controller.MemberController.join(..))")
+        public void joinPoint(){}
+
+
     // around 있는 부분이 Advice.. '*' 은 접근제한
-    @Around("execution(* hi1237.hello.spring4.controller.MemberController.myinfo(..))")
+    // pointcut과 around를 합쳐서 advisor...라고 부른다
+    @Around("myInfoPoint()")
     public Object myinfoAOPProcess(ProceedingJoinPoint pjp) throws Throwable {
 
         logger.info("myinfoAOPProcess 호출!!");
@@ -39,4 +48,30 @@ public class MemberServiceAdvice {
         Object obj = pjp.proceed();
         return obj;
     }
+
+    // 아래 around는 advice...
+    @Around("joinPoint()")
+    public Object joinAOPProcess(ProceedingJoinPoint pjp) throws Throwable {
+
+        logger.info("joinAOPProcess 호출!!");
+        HttpSession sess = null;
+
+        // 이 부분에서는 MemberController의 join(GET) 부분에 HttpSession이 없어서 null 값이 떴음
+        // 그래서 String join에 HttpSession 값을 넣어줘서 logger가 아닌 HttpSession이 작동하도록 함.
+        for(Object o : pjp.getArgs()){
+            if(o instanceof HttpSession)
+                sess= (HttpSession) o;
+        }
+
+        // 포인트컷 대상 메서드 실행
+        // 로그인 안했다면 -> join 으로 가고
+        // 로그인 했다면 -> myinfo로 이동
+        if (sess.getAttribute("member") != null)
+            return "redirect:/member/myinfo";
+
+        //
+        Object obj = pjp.proceed();
+        return obj;
+    }
+
 }
