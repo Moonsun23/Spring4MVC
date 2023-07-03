0347966c3d1246a2bc1a71aa2c127f6ffcdc9c83
Moonsun23 <socoooool90@gmail.com>
2023-07-03 14:51:47 +0900

    boardDAO: 조회수 증가, insertBoard 구현

--- src/main/java/hi1237/hello/spring4/dao/BoardDAO.java
+++ src/main/java/hi1237/hello/spring4/dao/BoardDAO.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface BoardDAO {
     List<Board> selectBoard(int snum);
     Board selectOneBoard(String bno);
+
+    int insertBoard(Board bd);
+    // sql의 board (title, userid, contents) 부분을 Board bd로 가져옴. 클래스화 하여
 }
